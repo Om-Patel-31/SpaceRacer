983f48b121415cfe462cfb7a5700116826f5ffec
Om Patel <ompate538@ed.amdsb.ca>
2025-01-13 11:30:47 -0500

    Add Timer and Label for countdown; update spaceship graphics
    Added a Timer component to handle countdown functionality, with a
    tick interval of 1 second, and an associated event handler to
    update the countdown and refresh the form. Introduced a Label
    component to display the remaining time, with specific font and
    location settings. Adjusted the form's client size and included
    the new Label in the form's controls. Added global variables for
    managing the time bar's height and graphical elements. Changed
    the `time` variable type from double to int and its initial value
    from 540 to 120. Updated the spaceship's body and thrust
    rectangles' positions and dimensions. Modified the `Form1_Paint`
    method to draw the new graphical elements. Added metadata for the
    Timer component in the `Form1.resx` file.

--- SpaceRacer/Form1.Designer.cs
+++ SpaceRacer/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.components = new System.ComponentModel.Container();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.timeBarTimer = new System.Windows.Forms.Timer(this.components);
+            this.outputLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // gameTimer
@@ -38,12 +40,28 @@
             this.gameTimer.Interval = 16;
             this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
             // 
+            // timeBarTimer
+            // 
+            this.timeBarTimer.Enabled = true;
+            this.timeBarTimer.Interval = 1000;
+            this.timeBarTimer.Tick += new System.EventHandler(this.timeBarTimer_Tick);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.outputLabel.Location = new System.Drawing.Point(559, 67);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 39);
+            this.outputLabel.TabIndex = 0;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(1358, 721);
+            this.ClientSize = new System.Drawing.Size(1335, 740);
+            this.Controls.Add(this.outputLabel);
             this.DoubleBuffered = true;
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -51,12 +69,15 @@
             this.Text = "Space Racer";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Timer timeBarTimer;
+        private System.Windows.Forms.Label outputLabel;
     }
 }
 
--- SpaceRacer/Form1.cs
+++ SpaceRacer/Form1.cs
@@ -13,42 +13,62 @@ namespace SpaceRacer
     public partial class Form1 : Form
     {
         // Global Variables
-        double time = 540;
+        int time = 120;
+        int timeBarHeight = 600;
 
-        //Rectangle spaceShip1 = new Rectangle(10, 10, 60, 60);
-        Rectangle spaceShipBody = new Rectangle(250 + 25, 530, 10, 15);
-        Rectangle thrustOne = new Rectangle(248 + 25, 550, 15, 3);
+        Rectangle spaceShip1 = new Rectangle(275, 10, 60, 60);
+        Rectangle spaceShip1Body = new Rectangle(275, 565, 10, 15);
+        Rectangle spaceShip2Body = new Rectangle(275 + 400, 565, 10, 15);
+        Rectangle thrustOne = new Rectangle(273, 585, 15, 3);
         Rectangle timeBar;
         //Rectangle thrustTwo = new Rectangle(255, 550, 7, 3);
+        Rectangle timeBarOuter = new Rectangle(475, 0, 10, 600);
+        Rectangle difference;
 
         //Brushes
         SolidBrush spaceShipBrush = new SolidBrush(Color.White);
+        Pen Pen = new Pen(Color.White);
 
         private void gameTimer_Tick(object sender, EventArgs e)
         {
-            time -= 4.5;
+        }
+        
+        private void timeBarTimer_Tick(object sender, EventArgs e)
+        {
+            //timeBarTimer.Enabled = true;
+            time-=1;
+            timeBarHeight -= 5;
+            outputLabel.Text = "Time: " + time;
+            Refresh();
         }
 
         public Form1()
         {
             InitializeComponent();
-            timeBar = new Rectangle(475, 40, 10, Convert.ToInt32(time));
+            difference = new Rectangle(timeBarOuter.X + spaceShip1Body.Width, spaceShip1Body.Y, timeBarOuter.X - spaceShip1Body.X - timeBarOuter.Width, 30);
         }
-
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            spaceShipBrush.Color = Color.White;
+            timeBar = new Rectangle(475, 0, 10, timeBarHeight);
             //body
-            e.Graphics.FillRectangle(spaceShipBrush, spaceShipBody);
+            e.Graphics.FillRectangle(spaceShipBrush, spaceShip1Body);
+            e.Graphics.FillRectangle(spaceShipBrush, spaceShip2Body);
             //top
-            e.Graphics.FillPolygon(spaceShipBrush, new PointF[] { new PointF(255 + 25, 520), new PointF(265 + 25, 530), new PointF(245 + 25, 530) });
+            e.Graphics.FillPolygon(spaceShipBrush, new PointF[] { new PointF(280, 555), new PointF(290, 565), new PointF(270, 565) });
             //bottom
-            e.Graphics.FillPolygon(spaceShipBrush, new PointF[] { new PointF(255 + 25, 535), new PointF(267 + 25, 550), new PointF(243 + 25, 550) });
+            e.Graphics.FillPolygon(spaceShipBrush, new PointF[] { new PointF(280, 570), new PointF(292, 585), new PointF(268, 585) });
             //thrust1
             e.Graphics.FillRectangle(spaceShipBrush, thrustOne);
-            //time
-            spaceShipBrush.Color = Color.Green;
+            //outer time bar
+            e.Graphics.DrawRectangle(Pen, timeBarOuter);
+            //time bar
             e.Graphics.FillRectangle(spaceShipBrush, timeBar);
+            //difference
+            spaceShipBrush.Color = Color.HotPink;
+            e.Graphics.FillRectangle(spaceShipBrush, difference);
         }
+
     }
 }
