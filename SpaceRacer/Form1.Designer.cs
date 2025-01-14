3ea30dd49af28f5809dce50cc33f3944857459fb
Om Patel <ompate538@ed.amdsb.ca>
2025-01-14 11:29:12 -0500

    Add score labels, key handlers, and collision detection
    - Added `p1ScoreLabel` and `p2ScoreLabel` to display player scores.
    - Changed `timeBarTimer` interval from 1000ms to 500ms.
    - Set `outputLabel` background color to transparent.
    - Initialized properties for `p1ScoreLabel` and `p2ScoreLabel`.
    - Added `p1ScoreLabel` and `p2ScoreLabel` to form controls.
    - Added `KeyDown` and `KeyUp` event handlers for player controls.
    - Reduced initial game time from 120s to 60s.
    - Added boolean variables to track key presses for players.
    - Added `Random` object for generating random values.
    - Modified initial positions and sizes of spaceships.
    - Added lists to store obstacles and their speeds.
    - Updated `gameTimer_Tick` to move spaceships and handle obstacles.
    - Added collision detection and spaceship position reset on collision.
    - Updated `Form1_Paint` to draw spaceships and obstacles.
    - Removed `difference` rectangle and its drawing code.

--- SpaceRacer/Form1.Designer.cs
+++ SpaceRacer/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
             this.timeBarTimer = new System.Windows.Forms.Timer(this.components);
             this.outputLabel = new System.Windows.Forms.Label();
+            this.p1ScoreLabel = new System.Windows.Forms.Label();
+            this.p2ScoreLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // gameTimer
@@ -43,17 +45,38 @@
             // timeBarTimer
             // 
             this.timeBarTimer.Enabled = true;
-            this.timeBarTimer.Interval = 1000;
+            this.timeBarTimer.Interval = 500;
             this.timeBarTimer.Tick += new System.EventHandler(this.timeBarTimer_Tick);
             // 
             // outputLabel
             // 
             this.outputLabel.AutoSize = true;
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
             this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
             this.outputLabel.Location = new System.Drawing.Point(559, 67);
             this.outputLabel.Name = "outputLabel";
             this.outputLabel.Size = new System.Drawing.Size(0, 39);
             this.outputLabel.TabIndex = 0;
+            // 
+            // p1ScoreLabel
+            // 
+            this.p1ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.p1ScoreLabel.Font = new System.Drawing.Font("Agency FB", 50F);
+            this.p1ScoreLabel.Location = new System.Drawing.Point(453, 633);
+            this.p1ScoreLabel.Name = "p1ScoreLabel";
+            this.p1ScoreLabel.Size = new System.Drawing.Size(157, 98);
+            this.p1ScoreLabel.TabIndex = 1;
+            this.p1ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // p2ScoreLabel
+            // 
+            this.p2ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.p2ScoreLabel.Font = new System.Drawing.Font("Agency FB", 50F);
+            this.p2ScoreLabel.Location = new System.Drawing.Point(686, 633);
+            this.p2ScoreLabel.Name = "p2ScoreLabel";
+            this.p2ScoreLabel.Size = new System.Drawing.Size(156, 98);
+            this.p2ScoreLabel.TabIndex = 2;
+            this.p2ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
@@ -61,6 +84,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(1335, 740);
+            this.Controls.Add(this.p2ScoreLabel);
+            this.Controls.Add(this.p1ScoreLabel);
             this.Controls.Add(this.outputLabel);
             this.DoubleBuffered = true;
             this.ForeColor = System.Drawing.Color.White;
@@ -68,6 +93,8 @@
             this.Name = "Form1";
             this.Text = "Space Racer";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,6 +105,8 @@
         private System.Windows.Forms.Timer gameTimer;
         private System.Windows.Forms.Timer timeBarTimer;
         private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label p1ScoreLabel;
+        private System.Windows.Forms.Label p2ScoreLabel;
     }
 }
 
--- SpaceRacer/Form1.cs
+++ SpaceRacer/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,29 +14,199 @@ namespace SpaceRacer
     public partial class Form1 : Form
     {
         // Global Variables
-        int time = 120;
+        int time = 60;
         int timeBarHeight = 600;
 
-        Rectangle spaceShip1 = new Rectangle(275, 10, 60, 60);
+        int obstacleWidth = 10;
+        int obstacleHeight = 3;
+
+        bool upPressed = false;
+        bool downPressed = false;
+        bool wPressed = false;
+        bool sPressed = false;
+
+        Random randGen = new Random();
+
+        Rectangle spaceShip1 = new Rectangle(270, 555, 20, 33);
         Rectangle spaceShip1Body = new Rectangle(275, 565, 10, 15);
-        Rectangle spaceShip2Body = new Rectangle(275 + 400, 565, 10, 15);
+        Rectangle spaceShip2 = new Rectangle(670, 555, 20, 33);
+        Rectangle spaceShip2Body = new Rectangle(675, 565, 10, 15);
         Rectangle thrustOne = new Rectangle(273, 585, 15, 3);
+        Rectangle thrustTwo = new Rectangle(673, 585, 15, 3);
         Rectangle timeBar;
-        //Rectangle thrustTwo = new Rectangle(255, 550, 7, 3);
         Rectangle timeBarOuter = new Rectangle(475, 0, 10, 600);
-        Rectangle difference;
+
+        //SpaceShip1 triangle points
+        PointF topTrianglep1 = new PointF(280, 555);
+        PointF topTrianglep2 = new PointF(290, 565);
+        PointF topTrianglep3 = new PointF(270, 565);
+        PointF lowTrianglep1 = new PointF(280, 570);
+        PointF lowTrianglep2 = new PointF(292, 585);
+        PointF lowTrianglep3 = new PointF(268, 585);
+
+        //SpaceShip2 triangle points
+        PointF topTrianglep4 = new PointF(680, 555);
+        PointF topTrianglep5 = new PointF(690, 565);
+        PointF topTrianglep6 = new PointF(670, 565);
+        PointF lowTrianglep4 = new PointF(680, 570);
+        PointF lowTrianglep5 = new PointF(692, 585);
+        PointF lowTrianglep6 = new PointF(668, 585);
+
+        List<Rectangle> obstacles = new List<Rectangle>();
+        List<int> obstacleSpeeds = new List<int>();
 
         //Brushes
         SolidBrush spaceShipBrush = new SolidBrush(Color.White);
         Pen Pen = new Pen(Color.White);
 
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    upPressed = false;
+                    break;
+                case Keys.Down:
+                    downPressed = false;
+                    break;
+                case Keys.W:
+                    wPressed = false;
+                    break;
+                case Keys.S:
+                    sPressed = false;
+                    break;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    upPressed = true;
+                    break;
+                case Keys.Down:
+                    downPressed = true;
+                    break;
+                case Keys.W:
+                    wPressed = true;
+                    break;
+                case Keys.S:
+                    sPressed = true;
+                    break;
+            }
+        }
+
         private void gameTimer_Tick(object sender, EventArgs e)
         {
+
+            if (wPressed == true)
+            {
+                spaceShip1.Y -= 3;
+                spaceShip1Body.Y -= 3;
+                topTrianglep1.Y -= 3;
+                topTrianglep2.Y -= 3;
+                topTrianglep3.Y -= 3;
+                lowTrianglep1.Y -= 3;
+                lowTrianglep2.Y -= 3;
+                lowTrianglep3.Y -= 3;
+                thrustOne.Y -= 3;
+            }
+
+            if (sPressed == true)
+            {
+                spaceShip1.Y += 3;
+                spaceShip1Body.Y += 3;
+                topTrianglep1.Y += 3;
+                topTrianglep2.Y += 3;
+                topTrianglep3.Y += 3;
+                lowTrianglep1.Y += 3;
+                lowTrianglep2.Y += 3;
+                lowTrianglep3.Y += 3;
+                thrustOne.Y += 3;
+            }
+
+            if (upPressed == true)
+            {
+                spaceShip2.Y -= 3;
+                spaceShip2Body.Y -= 3;
+                topTrianglep4.Y -= 3;
+                topTrianglep5.Y -= 3;
+                topTrianglep6.Y -= 3;
+                lowTrianglep4.Y -= 3;
+                lowTrianglep5.Y -= 3;
+                lowTrianglep6.Y -= 3;
+                thrustTwo.Y -= 3;
+            }
+
+            if (downPressed == true)
+            {
+                spaceShip2.Y += 3;
+                spaceShip2Body.Y += 3;
+                topTrianglep4.Y += 3;
+                topTrianglep5.Y += 3;
+                topTrianglep6.Y += 3;
+                lowTrianglep4.Y += 3;
+                lowTrianglep5.Y += 3;
+                lowTrianglep6.Y += 3;
+                thrustTwo.Y += 3;
+            }
+
+            int randValue = randGen.Next(1, 101);
+
+            if (randValue < 4)
+            {
+                int y = randGen.Next(0, this.Width - 50);
+                Rectangle obstacle = new Rectangle(0, y, obstacleWidth, obstacleHeight);
+                obstacles.Add(obstacle);
+                obstacleSpeeds.Add(randGen.Next(1, 6));
+            }
+            else if (randValue < 11)
+            {
+                int y = randGen.Next(0, this.Width - 50);
+                Rectangle obstacle = new Rectangle(0, y, obstacleWidth, obstacleHeight);
+                obstacles.Add(obstacle);
+                obstacleSpeeds.Add(randGen.Next(1, 6));
+            }
+            else if (randValue < 17)
+            {
+                int y = randGen.Next(0, this.Width - 50);
+                Rectangle obstacle = new Rectangle(0, y, obstacleWidth, obstacleHeight);
+                obstacles.Add(obstacle);
+                obstacleSpeeds.Add(randGen.Next(1, 6));
+            }
+
+            for (int i = 0; i < obstacles.Count; i++)
+            {
+                int x = obstacles[i].X + obstacleSpeeds[i];
+                obstacles[i] = new Rectangle(x, obstacles[i].Y, obstacleWidth, obstacleHeight);
+            }
+
+            for (int i = 0; i < obstacles.Count; i++)
+            {
+                if (spaceShip1.IntersectsWith(obstacles[i]))
+                {
+                    spaceShip1 = new Rectangle(270, 555, 20, 33);
+                    spaceShip1Body = new Rectangle(275, 565, 10, 15);
+                    topTrianglep1 = new PointF(280, 555);
+                    topTrianglep2 = new PointF(290, 565);
+                    topTrianglep3 = new PointF(270, 565);
+                    lowTrianglep1 = new PointF(280, 570);
+                    lowTrianglep2 = new PointF(292, 585);
+                    lowTrianglep3 = new PointF(268, 585);
+                    thrustOne = new Rectangle(273, 585, 15, 3);
+
+                    obstacles.RemoveAt(i);
+                    obstacleSpeeds.RemoveAt(i);
+                }
+            }
+
+            Refresh();
         }
-        
+
+
         private void timeBarTimer_Tick(object sender, EventArgs e)
         {
-            //timeBarTimer.Enabled = true;
             time-=1;
             timeBarHeight -= 5;
             outputLabel.Text = "Time: " + time;
@@ -45,7 +216,6 @@ namespace SpaceRacer
         public Form1()
         {
             InitializeComponent();
-            difference = new Rectangle(timeBarOuter.X + spaceShip1Body.Width, spaceShip1Body.Y, timeBarOuter.X - spaceShip1Body.X - timeBarOuter.Width, 30);
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -56,18 +226,25 @@ namespace SpaceRacer
             e.Graphics.FillRectangle(spaceShipBrush, spaceShip1Body);
             e.Graphics.FillRectangle(spaceShipBrush, spaceShip2Body);
             //top
-            e.Graphics.FillPolygon(spaceShipBrush, new PointF[] { new PointF(280, 555), new PointF(290, 565), new PointF(270, 565) });
+            e.Graphics.FillPolygon(spaceShipBrush, new PointF[] { topTrianglep1, topTrianglep2, topTrianglep3});
             //bottom
-            e.Graphics.FillPolygon(spaceShipBrush, new PointF[] { new PointF(280, 570), new PointF(292, 585), new PointF(268, 585) });
+            e.Graphics.FillPolygon(spaceShipBrush, new PointF[] { lowTrianglep1, lowTrianglep2, lowTrianglep3 });
+            //top
+            e.Graphics.FillPolygon(spaceShipBrush, new PointF[] { topTrianglep4, topTrianglep5, topTrianglep6 });
+            //bottom
+            e.Graphics.FillPolygon(spaceShipBrush, new PointF[] { lowTrianglep4, lowTrianglep5, lowTrianglep6 });
             //thrust1
             e.Graphics.FillRectangle(spaceShipBrush, thrustOne);
+            e.Graphics.FillRectangle(spaceShipBrush, thrustTwo);
             //outer time bar
             e.Graphics.DrawRectangle(Pen, timeBarOuter);
             //time bar
             e.Graphics.FillRectangle(spaceShipBrush, timeBar);
-            //difference
-            spaceShipBrush.Color = Color.HotPink;
-            e.Graphics.FillRectangle(spaceShipBrush, difference);
+            //obstacles (asteroidds)
+            for (int i = 0; i < obstacles.Count; i++)
+            {
+                e.Graphics.FillEllipse(spaceShipBrush, obstacles[i]);
+            }
         }
 
     }
